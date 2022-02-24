7ddc7175f8544228843bcf232c3fb63d8eeef63a
lokeshpeethani2 <lokeshpeethani2@gmail.com>
2022-02-24 13:19:22 +0000

    model and auth-viewmodel added

---
+++ Hospital-Management-API/Model/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_API.Model
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public int PatientAge { get; set; }
+        public string Department { get; set; }
+        public string DoctorName { get; set; }
+        public string DoctorFee { get; set; }
+        public string CreatedBy { get; set; }
+
+    }
+}
